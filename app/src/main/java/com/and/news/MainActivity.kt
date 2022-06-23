e0c40ac5e8b0dcd94412a8296bc68113fd6c4e0b
Zulfikar Ramadhan <ramadhan.fiki2@gmail.com>
2022-06-23 18:14:21 +0700

    test PR

--- app/src/main/java/com/and/news/MainActivity.kt
+++ app/src/main/java/com/and/news/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+  
+//    ini branch test untuk PR
+    
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
