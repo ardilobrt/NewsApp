dcb2f79573e33a1716d508bf98bff69b0d9a947e
ardilobrt <ardilobrt@gmail.com>
2022-06-23 16:06:43 +0700

    Add view binding

--- app/src/main/java/com/and/news/MainActivity.kt
+++ app/src/main/java/com/and/news/MainActivity.kt
@@ -2,10 +2,20 @@ package com.and.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.and.news.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
