012fec93b48bf9b0d80e9bdd6160b1f3cc3bdeb1
ardilobrt <ardilobrt@gmail.com>
2022-06-24 12:46:58 +0700

    Add Layout Bottom Nav

---
+++ app/src/main/java/com/and/news/ui/bookmark/BookmarkFragment.kt
@@ -0,0 +1,42 @@
+package com.and.news.ui.bookmark
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.and.news.databinding.FragmentBookmarkBinding
+
+class BookmarkFragment : Fragment() {
+
+    private var _binding: FragmentBookmarkBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val bookmarkViewModel =
+            ViewModelProvider(this).get(BookmarkViewModel::class.java)
+
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textBookmark
+        bookmarkViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/and/news/ui/bookmark/BookmarkViewModel.kt
@@ -0,0 +1,13 @@
+package com.and.news.ui.bookmark
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BookmarkViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
