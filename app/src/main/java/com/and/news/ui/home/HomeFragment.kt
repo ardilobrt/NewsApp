7d4877cbafdee47fef60ffe45590b8206dcb802d
Juna <ardilobrt@gmail.com>
2022-06-30 22:41:25 +0700

    Implement function show recycle view, loading, & refreshing data

--- app/src/main/java/com/and/news/ui/home/HomeFragment.kt
+++ app/src/main/java/com/and/news/ui/home/HomeFragment.kt
@@ -5,12 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.and.news.R
+import com.and.news.adapter.ArticlesAdapter
+import com.and.news.data.MyCompanion.showLoading
+import com.and.news.data.model.ArticlesItem
 import com.and.news.databinding.FragmentHomeBinding
+import com.faltenreich.skeletonlayout.applySkeleton
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +28,39 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val layoutManager = LinearLayoutManager(context)
+        val itemDecoration = DividerItemDecoration(context, layoutManager.orientation)
+
+        binding.apply {
+            rvNews.layoutManager = layoutManager
+            rvNews.addItemDecoration(itemDecoration)
+        }
+
+        viewModel.setArticles()
+
+        binding.srlNews.setOnRefreshListener {
+            viewModel.setArticles()
+        }
+
+        viewModel.listArticles.observe(viewLifecycleOwner) {
+            getArticles(it)
+        }
+
+        viewModel.isLoading.observe(viewLifecycleOwner) {
+            binding.rvNews.showLoading(it)
+        }
+
+    }
+
+    private fun getArticles(listArticles: List<ArticlesItem>) {
+        val adapter = ArticlesAdapter(listArticles)
+        adapter.setList()
+        binding.rvNews.adapter = adapter
+        binding.srlNews.isRefreshing = false
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/and/news/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/and/news/ui/home/HomeViewModel.kt
@@ -1,13 +1,51 @@
 package com.and.news.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.and.news.api.ApiConfig
+import com.and.news.data.model.ArticlesItem
+import com.and.news.data.model.ArticlesResponse
+import com.faltenreich.skeletonlayout.Skeleton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _listArticles = MutableLiveData<List<ArticlesItem>>()
+    val listArticles: LiveData<List<ArticlesItem>> get() = _listArticles
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    fun setArticles() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getNewsByLocal(LOCAL)
+        client.enqueue(object : Callback<ArticlesResponse> {
+            override fun onResponse(
+                call: Call<ArticlesResponse>,
+                response: Response<ArticlesResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) _listArticles.value = responseBody.articles
+                } else Log.e(TAG, "onFailure: ${response.message()}", )
+            }
+
+            override fun onFailure(call: Call<ArticlesResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+
+        })
     }
-    val text: LiveData<String> = _text
+
+    companion object {
+        private const val TAG = "HomeFragment"
+        private const val LOCAL = "id"
+    }
+
 }
