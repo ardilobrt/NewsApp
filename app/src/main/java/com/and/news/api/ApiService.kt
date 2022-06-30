fe67067daa2e94b8763e86581248725c58fb5b56
Juna <ardilobrt@gmail.com>
2022-06-30 22:38:01 +0700

    Add API config & API service endpoint

---
+++ app/src/main/java/com/and/news/api/ApiService.kt
@@ -0,0 +1,18 @@
+package com.and.news.api
+
+import com.and.news.BuildConfig
+import com.and.news.data.MyCompanion
+import com.and.news.data.model.ArticlesResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("v2/top-headlines")
+    fun getNewsByLocal(
+        @Query("country") country: String,
+        @Query("apiKey") apikey: String = MyCompanion.API_KEY
+    ) : Call<ArticlesResponse>
+}
