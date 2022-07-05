e469d1233e83335457242e30a72f0dfe25da9543
CitChandra <cit.chandra.w@gmail.com>
2022-07-05 09:52:23 +0700

    Fixing string get started dan hello blank fragment menjadi translatable

---
+++ app/src/main/java/com/and/news/adapter/ViewPageAdapter.kt
@@ -0,0 +1,24 @@
+package com.and.news.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.and.news.ui.onboarding.fragment.FirstOnBoardingFragment
+import com.and.news.ui.onboarding.fragment.SecondOnBoardingFragment
+import com.and.news.ui.onboarding.fragment.ThirdOnBoardingFragment
+
+class ViewPagerAdapter(fm: FragmentManager, lf: Lifecycle) : FragmentStateAdapter(fm, lf) {
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? =null
+        when (position) {
+            0 -> fragment = FirstOnBoardingFragment()
+            1 -> fragment = SecondOnBoardingFragment()
+            2 -> fragment = ThirdOnBoardingFragment()
+        }
+        return fragment as Fragment
+    }
+
+}
---
+++ app/src/main/java/com/and/news/ui/onboarding/fragment/FirstOnBoardingFragment.kt
@@ -0,0 +1,6 @@
+package com.and.news.ui.onboarding.fragment
+
+import androidx.fragment.app.Fragment
+
+class FirstOnBoardingFragment : Fragment() {
+}
