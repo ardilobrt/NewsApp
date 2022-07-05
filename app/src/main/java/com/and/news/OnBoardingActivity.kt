4fae6946904c418c1af75dbabddaf515d844a814
CitChandra <cit.chandra.w@gmail.com>
2022-07-05 14:24:04 +0700

    Create Logic On Boarding

---
+++ app/src/main/java/com/and/news/OnBoardingActivity.kt
@@ -0,0 +1,86 @@
+package com.and.news
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.WindowInsetsController
+import com.and.news.adapter.ViewPagerAdapter
+import com.and.news.data.SharedPrefManager
+import com.and.news.databinding.ActivityOnBoardingBinding
+import java.util.*
+import kotlin.concurrent.schedule
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+    private var contentHasLoaded = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Timer().schedule(2000) {
+            contentHasLoaded = true
+        }
+        setupSplashScreen()
+
+        setStatusBar(false)
+
+        val isOnBoardingShow = SharedPrefManager.getIsOnBoardingShown(this)
+        if (isOnBoardingShow) goToMain()
+
+        binding.apply {
+            viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+            dotsIndicator.attachTo(viewPager)
+        }
+
+        binding.btnNext.setOnClickListener {
+            setStatusBar(true)
+            SharedPrefManager.setIsOnBoardingShown(this, true)
+            goToMain()
+        }
+    }
+
+    // Setup Splash Screen
+    private fun setupSplashScreen() {
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (contentHasLoaded) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else false
+                }
+            }
+        )
+    }
+
+    // Setup Action Bar
+    private fun setStatusBar(isFlagCleared: Boolean) {
+        supportActionBar?.hide()
+        window.statusBarColor = Color.TRANSPARENT
+        if (!isFlagCleared) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            window.insetsController?.setSystemBarsAppearance(
+                0,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        }
+    }
+
+    private fun goToMain() {
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/and/news/ui/onboarding/fragment/SecondOnBoardingFragment.kt
+++ app/src/main/java/com/and/news/ui/onboarding/fragment/SecondOnBoardingFragment.kt
@@ -1,6 +1,32 @@
 package com.and.news.ui.onboarding.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.and.news.R
+import com.and.news.databinding.FragmentScreenOnBoardingBinding
 
 class SecondOnBoardingFragment : Fragment() {
+    private var _binding: FragmentScreenOnBoardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentScreenOnBoardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.ivOnboarding.setImageResource(R.drawable.on_boarding_2)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
