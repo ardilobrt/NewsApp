43f67149a98ab46f912a5ba1b0cc7640f6d30284
Juna <ardilobrt@gmail.com>
2022-07-06 16:49:17 +0700

    Add method login with ORM ROOM & MVP

--- app/src/main/java/com/and/news/ui/auth/login/SignInActivity.kt
+++ app/src/main/java/com/and/news/ui/auth/login/SignInActivity.kt
@@ -3,22 +3,64 @@ package com.and.news.ui.auth.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.and.news.data.database.UserDatabase
 import com.and.news.databinding.ActivitySignInBinding
 import com.and.news.ui.auth.register.SignUpActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity(), SignInView {
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var presenter: SignInPresenterImp
+    private var database: UserDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
+        presenter = SignInPresenterImp(this)
         setContentView(binding.root)
 
-        binding.btnDontHaveAccount.setOnClickListener {
-            Intent(this, SignUpActivity::class.java).also { intent ->
+        database = UserDatabase.getInstance(this)
+
+        setUiListener()
+    }
+
+    private fun setUiListener() = with(binding) {
+
+        btnSignIn.setOnClickListener {
+            val email = inputEmail.text.toString()
+            val password = inputPassword.text.toString()
+
+            if (presenter.checkEmpty(email, password)) checkUserDatabase(email, password)
+        }
+
+        btnDontHaveAccount.setOnClickListener {
+            Intent(this@SignInActivity, SignUpActivity::class.java).also { intent ->
                 startActivity(intent)
                 finish()
             }
         }
     }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun checkUserDatabase(email: String, password: String) {
+
+        GlobalScope.launch {
+            val result = database?.userDao()?.getUsername(email)
+
+            runOnUiThread {
+                if (result != null) {
+                    if (presenter.validateUser(result, email, password)) finish()
+                } else showMessage("User Cannot Found")
+            }
+        }
+    }
+
+    override fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+
 }
--- app/src/main/java/com/and/news/ui/auth/login/SignInPresenter.kt
+++ app/src/main/java/com/and/news/ui/auth/login/SignInPresenter.kt
@@ -1,8 +1,12 @@
 package com.and.news.ui.auth.login
 
+import com.and.news.data.entity.Users
+
 interface SignInPresenter {
 
-    fun checkEmpty(email: String, password: String) : Boolean
+    fun checkEmpty(email: String, password: String): Boolean
+
+    fun validateUser(users: Users, email: String, password: String): Boolean
 
     fun setSuccess(isSuccess: Boolean)
 }
