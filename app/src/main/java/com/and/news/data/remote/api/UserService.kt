ecea51be5e8c047c97b1eeeccc354088342caaf4
Juna <ardilobrt@gmail.com>
2022-07-23 11:30:21 +0700

    Initial commit

--- app/src/main/java/com/and/news/data/remote/api/UserService.kt
+++ app/src/main/java/com/and/news/data/remote/api/UserService.kt
@@ -1,5 +1,6 @@
 package com.and.news.data.remote.api
 
+import com.and.news.data.remote.model.SignInResponse
 import com.and.news.data.remote.model.SignUpResponse
 import retrofit2.Call
 import retrofit2.http.Body
@@ -9,4 +10,7 @@ interface UserService {
 
     @POST("auth/register")
     fun registerUser(@Body signUpResponse: SignUpResponse) : Call<SignUpResponse>
+
+    @POST("auth/login")
+    fun loginUser(@Body signInResponse: SignInResponse) : Call<SignInResponse>
 }
