e26f8205268ed32e8f36479ee136cbf6fb0b8381
Juna <ardilobrt@gmail.com>
2022-07-23 10:50:32 +0700

    Initial commit

--- app/src/main/java/com/and/news/ui/auth/register/SignUpActivity.kt
+++ app/src/main/java/com/and/news/ui/auth/register/SignUpActivity.kt
@@ -1,31 +1,19 @@
 package com.and.news.ui.auth.register
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.and.news.data.local.room.UserDatabase
-import com.and.news.data.local.entity.Users
+import androidx.appcompat.app.AppCompatActivity
 import com.and.news.databinding.ActivitySignUpBinding
 import com.and.news.ui.auth.login.SignInActivity
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
-class SignUpActivity : AppCompatActivity(), SignUpView {
+class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
-    private lateinit var presenter: SignUpPresenterImp
-    private lateinit var users: Users
-    private var database: UserDatabase? = null
 
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
-        presenter = SignUpPresenterImp(this)
         setContentView(binding.root)
-
-        database = UserDatabase.getInstance(this)
 
         binding.apply {
 
@@ -34,19 +22,7 @@ class SignUpActivity : AppCompatActivity(), SignUpView {
                 val email = inputEmail.text.toString()
                 val password = inputPassword.text.toString()
 
-                if (presenter.checkEmpty(username, email, password)) {
-                    users = Users(null, username, email, password)
 
-                    GlobalScope.launch {
-                        val result = database?.userDao()?.insertUser(users)
-                        runOnUiThread {
-                            if (result != 0.toLong()) {
-                                presenter.setSuccess(true)
-                                goToSignIn()
-                            } else presenter.setSuccess(false)
-                        }
-                    }
-                }
             }
 
             btnHaveAccount.setOnClickListener {
@@ -62,18 +38,7 @@ class SignUpActivity : AppCompatActivity(), SignUpView {
         }
     }
 
-    override fun showMessage(message: String) {
+    fun showMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun clearField() {
-        binding.apply {
-            inputUserName.clearFocus()
-            inputEmail.clearFocus()
-            inputPassword.clearFocus()
-            inputUserName.text?.clear()
-            inputEmail.text?.clear()
-            inputPassword.text?.clear()
-        }
     }
 }
